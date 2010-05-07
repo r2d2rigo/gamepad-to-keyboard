8ebba589cf70c23f1dbfb8b7026ab7aed663eddd
r2d2rigo_cp <SND\r2d2rigo_cp>
2010-05-07 18:37:01 +0000

    Initial version.

---
+++ GP2KB/GP2KB.Designer.cs
@@ -0,0 +1,273 @@
+﻿/*
+ * GamePad to KeyBoard
+ * Copyright (c) 2010 Rodrigo 'r2d2rigo' Diaz
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+namespace GP2KB
+{
+    partial class GP2KB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GP2KB));
+            this.keyA = new System.Windows.Forms.TextBox();
+            this.keyX = new System.Windows.Forms.TextBox();
+            this.keyB = new System.Windows.Forms.TextBox();
+            this.keyY = new System.Windows.Forms.TextBox();
+            this.keyDown = new System.Windows.Forms.TextBox();
+            this.keyLeft = new System.Windows.Forms.TextBox();
+            this.keyRight = new System.Windows.Forms.TextBox();
+            this.keyUp = new System.Windows.Forms.TextBox();
+            this.keyBack = new System.Windows.Forms.TextBox();
+            this.keyStart = new System.Windows.Forms.TextBox();
+            this.keyLShoulder = new System.Windows.Forms.TextBox();
+            this.keyRShoulder = new System.Windows.Forms.TextBox();
+            this.toolTipHelp = new System.Windows.Forms.ToolTip(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // keyA
+            // 
+            this.keyA.Location = new System.Drawing.Point(610, 178);
+            this.keyA.Name = "keyA";
+            this.keyA.Size = new System.Drawing.Size(45, 20);
+            this.keyA.TabIndex = 1;
+            this.keyA.Tag = "A";
+            this.toolTipHelp.SetToolTip(this.keyA, "A Button");
+            this.keyA.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyX
+            // 
+            this.keyX.Location = new System.Drawing.Point(550, 121);
+            this.keyX.Name = "keyX";
+            this.keyX.Size = new System.Drawing.Size(41, 20);
+            this.keyX.TabIndex = 1;
+            this.keyX.Tag = "X";
+            this.toolTipHelp.SetToolTip(this.keyX, "X Button");
+            this.keyX.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyB
+            // 
+            this.keyB.Location = new System.Drawing.Point(668, 121);
+            this.keyB.Name = "keyB";
+            this.keyB.Size = new System.Drawing.Size(42, 20);
+            this.keyB.TabIndex = 1;
+            this.keyB.Tag = "B";
+            this.toolTipHelp.SetToolTip(this.keyB, "B Button");
+            this.keyB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyY
+            // 
+            this.keyY.Location = new System.Drawing.Point(610, 63);
+            this.keyY.Name = "keyY";
+            this.keyY.Size = new System.Drawing.Size(44, 20);
+            this.keyY.TabIndex = 1;
+            this.keyY.Tag = "Y";
+            this.toolTipHelp.SetToolTip(this.keyY, "Y Button");
+            this.keyY.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyDown
+            // 
+            this.keyDown.Location = new System.Drawing.Point(261, 292);
+            this.keyDown.Name = "keyDown";
+            this.keyDown.Size = new System.Drawing.Size(49, 20);
+            this.keyDown.TabIndex = 1;
+            this.keyDown.Tag = "DPadDown";
+            this.toolTipHelp.SetToolTip(this.keyDown, "Directional Pad Down");
+            this.keyDown.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyLeft
+            // 
+            this.keyLeft.Location = new System.Drawing.Point(201, 244);
+            this.keyLeft.Name = "keyLeft";
+            this.keyLeft.Size = new System.Drawing.Size(47, 20);
+            this.keyLeft.TabIndex = 1;
+            this.keyLeft.Tag = "DPadLeft";
+            this.toolTipHelp.SetToolTip(this.keyLeft, "Directional Pad Left");
+            this.keyLeft.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyRight
+            // 
+            this.keyRight.Location = new System.Drawing.Point(326, 244);
+            this.keyRight.Name = "keyRight";
+            this.keyRight.Size = new System.Drawing.Size(49, 20);
+            this.keyRight.TabIndex = 1;
+            this.keyRight.Tag = "DPadRight";
+            this.toolTipHelp.SetToolTip(this.keyRight, "Directional Pad Right");
+            this.keyRight.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyUp
+            // 
+            this.keyUp.Location = new System.Drawing.Point(261, 193);
+            this.keyUp.Name = "keyUp";
+            this.keyUp.Size = new System.Drawing.Size(49, 20);
+            this.keyUp.TabIndex = 1;
+            this.keyUp.Tag = "DPadUp";
+            this.toolTipHelp.SetToolTip(this.keyUp, "Directial Pad Up");
+            this.keyUp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyBack
+            // 
+            this.keyBack.Location = new System.Drawing.Point(297, 121);
+            this.keyBack.Name = "keyBack";
+            this.keyBack.Size = new System.Drawing.Size(49, 20);
+            this.keyBack.TabIndex = 1;
+            this.keyBack.Tag = "Back";
+            this.toolTipHelp.SetToolTip(this.keyBack, "Back Button");
+            this.keyBack.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyStart
+            // 
+            this.keyStart.Location = new System.Drawing.Point(464, 121);
+            this.keyStart.Name = "keyStart";
+            this.keyStart.Size = new System.Drawing.Size(49, 20);
+            this.keyStart.TabIndex = 1;
+            this.keyStart.Tag = "Start";
+            this.toolTipHelp.SetToolTip(this.keyStart, "Start Button");
+            this.keyStart.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyLShoulder
+            // 
+            this.keyLShoulder.Location = new System.Drawing.Point(152, 12);
+            this.keyLShoulder.Name = "keyLShoulder";
+            this.keyLShoulder.Size = new System.Drawing.Size(49, 20);
+            this.keyLShoulder.TabIndex = 1;
+            this.keyLShoulder.Tag = "LeftShoulder";
+            this.toolTipHelp.SetToolTip(this.keyLShoulder, "Left Shoulder Button");
+            this.keyLShoulder.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // keyRShoulder
+            // 
+            this.keyRShoulder.Location = new System.Drawing.Point(605, 12);
+            this.keyRShoulder.Name = "keyRShoulder";
+            this.keyRShoulder.Size = new System.Drawing.Size(49, 20);
+            this.keyRShoulder.TabIndex = 1;
+            this.keyRShoulder.Tag = "RightShoulder";
+            this.toolTipHelp.SetToolTip(this.keyRShoulder, "Right Shoulder Button");
+            this.keyRShoulder.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyButton_KeyDown);
+            // 
+            // toolTipHelp
+            // 
+            this.toolTipHelp.AutomaticDelay = 100;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "GP2KB";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Image = global::GP2KB.Properties.Resources.help;
+            this.buttonHelp.Location = new System.Drawing.Point(775, 547);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(24, 24);
+            this.buttonHelp.TabIndex = 2;
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GP2KB.Properties.Resources.controller;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(799, 571);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GP2KB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 572);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.keyRShoulder);
+            this.Controls.Add(this.keyLShoulder);
+            this.Controls.Add(this.keyStart);
+            this.Controls.Add(this.keyBack);
+            this.Controls.Add(this.keyUp);
+            this.Controls.Add(this.keyY);
+            this.Controls.Add(this.keyRight);
+            this.Controls.Add(this.keyB);
+            this.Controls.Add(this.keyLeft);
+            this.Controls.Add(this.keyDown);
+            this.Controls.Add(this.keyX);
+            this.Controls.Add(this.keyA);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "GP2KB";
+            this.Text = "GamePad to KeyBoard";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.GP2KB_FormClosed);
+            this.Resize += new System.EventHandler(this.GP2KB_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keyA;
+        private System.Windows.Forms.TextBox keyX;
+        private System.Windows.Forms.TextBox keyB;
+        private System.Windows.Forms.TextBox keyY;
+        private System.Windows.Forms.TextBox keyDown;
+        private System.Windows.Forms.TextBox keyLeft;
+        private System.Windows.Forms.TextBox keyRight;
+        private System.Windows.Forms.TextBox keyUp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox keyBack;
+        private System.Windows.Forms.TextBox keyStart;
+        private System.Windows.Forms.TextBox keyLShoulder;
+        private System.Windows.Forms.TextBox keyRShoulder;
+        private System.Windows.Forms.ToolTip toolTipHelp;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button buttonHelp;
+    }
+}
+
---
+++ GP2KB/GP2KB.cs
@@ -0,0 +1,209 @@
+﻿/*
+ * GamePad to KeyBoard
+ * Copyright (c) 2010 Rodrigo 'r2d2rigo' Diaz
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Threading;
+
+namespace GP2KB
+{
+    public partial class GP2KB : Form
+    {
+#region WinAPI function imports
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
+#endregion
+
+#region WinAPI Constants and data types
+        const int INPUT_KEYBOARD = 1;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+
+        public struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public long time;
+            public uint dwExtraInfo;
+        };
+
+        [StructLayout(LayoutKind.Explicit, Size = 28)]
+        public struct INPUT
+        {
+            [FieldOffset(0)]
+            public uint type;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+        };
+#endregion
+
+#region Variables
+        static GamePadState prevState, currState;
+        private Thread thread;
+
+        public static Semaphore semaphore;
+
+        public static Dictionary<string, string> keyButtons;
+#endregion
+
+#region Functions
+        /// <summary>
+        /// Initializes variables and starts thread.
+        /// </summary>
+        public GP2KB()
+        {
+            InitializeComponent();
+
+            prevState = currState = GamePad.GetState(PlayerIndex.One);
+
+            semaphore = new Semaphore(1, 1);
+            keyButtons = new Dictionary<string, string>();
+
+            thread = new Thread(new ThreadStart(InputCheck));
+            thread.Start();
+        }
+
+        /// <summary>
+        /// Background thread function. Manages gamepad input and send keyboard messages.
+        /// </summary>
+        static void InputCheck()
+        {
+            INPUT structInput = new INPUT();
+
+            while (true)
+            {
+                currState = GamePad.GetState(PlayerIndex.One);
+
+                structInput.type = INPUT_KEYBOARD;
+                structInput.ki.wScan = 0;
+                structInput.ki.time = 0;
+                structInput.ki.dwFlags = 0;
+
+                // Check that no one else is accessing the dictionary
+                semaphore.WaitOne();
+                foreach (KeyValuePair<string, string> keybutton in keyButtons)
+                {
+                    Buttons but = (Buttons)Enum.Parse(typeof(Buttons),keybutton.Key);
+                    System.Windows.Forms.Keys keyv = (System.Windows.Forms.Keys)Enum.Parse(typeof(System.Windows.Forms.Keys), keybutton.Value);
+                    ushort keyvalue = (ushort)keyv;
+
+                    if (currState.IsButtonDown(but) && !prevState.IsButtonDown(but))
+                    {
+                        structInput.ki.wVk = keyvalue;
+                        uint intReturn = SendInput(1, ref structInput, Marshal.SizeOf(new INPUT()));
+                        
+                    }
+                    if (!currState.IsButtonDown(but) && prevState.IsButtonDown(but))
+                    {
+                        structInput.ki.wVk = keyvalue;
+                        structInput.ki.dwFlags = KEYEVENTF_KEYUP;
+                        uint intReturn = SendInput(1, ref structInput, Marshal.SizeOf(new INPUT()));
+                    }
+                }
+                semaphore.Release();
+
+                prevState = currState;
+                Thread.Sleep(1);
+            }
+        }
+
+        /// <summary>
+        /// Application closing, stop background thread.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GP2KB_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thread.Abort();
+        }
+
+        /// <summary>
+        /// Key pressed on a TextBox, change keyboard key <-> gamepad button association.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void keyButton_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Check that no one else is accessing the dictionary
+            semaphore.WaitOne();
+            // Associated gamepad button is stored in TextBox.Tag
+            keyButtons[(string)((TextBox)sender).Tag] = e.KeyCode.ToString();
+            semaphore.Release();
+
+            ((TextBox)sender).Text = e.KeyCode.ToString();
+            // Don't propagate input
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+        }
+
+        /// <summary>
+        /// Notify icon clicked, show main window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!this.Visible)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.notifyIcon.Visible = false;
+            }
+        }
+
+        /// <summary>
+        /// Window minimized, hide and show notify icon.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GP2KB_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon.Visible = true;
+            }
+        }
+
+        /// <summary>
+        /// Shows about box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            AboutBox about = new AboutBox();
+            about.ShowDialog();
+        }
+#endregion
+    }
+}
